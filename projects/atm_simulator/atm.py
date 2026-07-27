424f8b03f62442c82f617d3b6dd010143c70a648
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-27 21:47:32 +0530

    Add ATM simulator projects

---
+++ projects/atm_simulator/atm.py
@@ -0,0 +1,36 @@
+print("===== ATM Simulator =====")
+
+pin = int(input("Enter PIN: "))
+
+if pin == 1234:
+
+    print("\n1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+
+    choice = int(input("Enter your choice: "))
+
+    balance = 10000
+
+    if choice == 1:
+        print("Your Balance is:", balance)
+
+    elif choice == 2:
+        amount = int(input("Enter deposit amount: "))
+        balance = balance + amount
+        print("New Balance:", balance)
+
+    elif choice == 3:
+        amount = int(input("Enter withdraw amount: "))
+
+        if amount <= balance:
+            balance = balance - amount
+            print("New Balance:", balance)
+        else:
+            print("Insufficient Balance")
+
+    else:
+        print("Invalid Choice")
+
+else:
+    print("Invalid PIN")
