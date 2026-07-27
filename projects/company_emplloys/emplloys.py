6e46fbf1b15ee7381cf0db5ca7949d1d0c888e36
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-27 15:35:22 +0530

    Update emplloys project

--- projects/company_emplloys/emplloys.py
+++ projects/company_emplloys/emplloys.py
@@ -1,9 +1,15 @@
 print("==========Emplloys Details==========")
+
 name = input("Enter your name: ")
 emplloy_ID = int(input"Enter your emplloy_ID: ")
 department =input("Which delartment are you in? "
 )
 salary = input("Enter your chrrent salary: ")
+if salary >= 5000:
+print("senior emplloy")
+else:
+print("junior emplloy")
+
  
 print("=====================================")
 
