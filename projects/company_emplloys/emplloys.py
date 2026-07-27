ff911a8c9b6c81ccbe495402fbc5f4d1c374f190
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-27 15:24:31 +0530

    Add amplloys projects

---
+++ projects/company_emplloys/emplloys.py
@@ -0,0 +1,9 @@
+print("==========Emplloys Details==========")
+name = input("Enter your name: ")
+emplloy_ID = int(input"Enter your emplloy_ID: ")
+department =input("Which delartment are you in? "
+)
+salary = input("Enter your chrrent salary: ")
+ 
+print("=====================================")
+
