711bdd68f81eac2d05f54261eedf32ceeacfa4b9
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-27 16:00:48 +0530

    fix emplloys information programm

--- projects/company_emplloys/emplloys.py
+++ projects/company_emplloys/emplloys.py
@@ -1,15 +1,13 @@
-print("==========Emplloys Details==========")
+print("===== Employee Information =====")
 
-name = input("Enter your name: ")
-emplloy_ID = int(input"Enter your emplloy_ID: ")
-department =input("Which delartment are you in? "
-)
-salary = input("Enter your chrrent salary: ")
-if salary >= 5000:
-print("senior emplloy")
-else:
-print("junior emplloy")
+employee_name = input("Enter your name: ")
+employee_ID = int(input("Enter your employee ID: "))
+department = input("Enter your department: ")
+salary = int(input("Enter your salary: "))
 
- 
-print("=====================================")
+print("\n----- Employee Details -----")
+print("Name      :", employee_name)
+print("ID        :", employee_ID)
+print("Department:", department)
+print("Salary    :", salary)
 
