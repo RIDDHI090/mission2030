76cb702e7ee0cbb2c8ff5b0da2e5f386938007b3
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-27 16:11:14 +0530

    Fix emplloys details projects

--- projects/company_emplloys/emplloys.py
+++ projects/company_emplloys/emplloys.py
@@ -4,6 +4,10 @@ employee_name = input("Enter your name: ")
 employee_ID = int(input("Enter your employee ID: "))
 department = input("Enter your department: ")
 salary = int(input("Enter your salary: "))
+if salary >= 5000:
+ print("seniour emplloy")
+else:
+ print("junior emplloy")
 
 print("\n----- Employee Details -----")
 print("Name      :", employee_name)
