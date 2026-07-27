cf279a7610b0445b3ed4192e930c865bd1750cd0
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-27 14:54:30 +0530

    Add student information project

---
+++ projects/student_info/student.py
@@ -0,0 +1,10 @@
+print("===== Student Information =====")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print("\n----- Result -----")
+print("Name :", name)
+print("Age  :", age)
+print("City :", city)
