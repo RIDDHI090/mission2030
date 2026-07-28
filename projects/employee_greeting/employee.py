883f49297cbf3a7818bda393b552b6cbbd2ae9f4
Riddhi Sabhadiya <riddhisabhadiya@gmail.com>
2026-07-28 13:54:55 +0530

    Add employee detail project

--- projects/employee_greeting/employee.py
+++ projects/employee_greeting/employee.py
@@ -1,16 +1,18 @@
-def employee_detail(name,department,salary):
+def employee_detail(name,department,salary,city):
     print("============================")
     
     print("Hello", name)
     print("Department", department)
     print("Salary", salary)
+    print("City", city)
     print("Welcome to 2030 company")
-
+    
     print("============================")
 
 employee = input("Enter employee name: ")
 department = input("Enter your department: ")
 salary = int(input("Enter your salary: "))
+city = input("Enter city: ")
 
-employee_detail(employee,department,salary)
-employee_detail("Pal","IT",20000)
+employee_detail(employee,department,salary,city)
+employee_detail("Pal","IT",20000,"surat")
